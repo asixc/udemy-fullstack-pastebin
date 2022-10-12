982f67fa919b99769b57a5934ac4b5dd7bc7ce1b
Jotxee <jotxee@jotxee.dev>
2022-10-12 18:59:12 +0200

    add backend template project

---
+++ backend/src/test/java/com/udemy/pastebinclon/PastebinClonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.pastebinclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
